0aa4a765e11baa2549e9124fb0044c3d82229f48
vinodgupta755 <146931402+vinodgupta755@users.noreply.github.com>
2025-01-07 13:46:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Select the audio element
+const song = document.getElementById('background-song');
+
+// Function to play the song
+function playSong() {
+    song.play();
+}
+
+// Function to pause the song
+function pauseSong() {
+    song.pause();
+}
